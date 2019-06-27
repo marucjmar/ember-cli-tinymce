14ca7dba12803bc2600e77c04a8ef76e3bd5ea8f
Ruy Rocha <admin@ruyrocha.com>
2019-06-27 18:48:07 -0300

    Add subresource integrity hash.

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ module.exports = {
     var content = '';
 
     if (type === 'head-footer' && config['tinyMCE'] && config['tinyMCE']['load']) {
-      var src = "//cdn.tinymce.com/"+ config['tinyMCE']['version'] +"/tinymce.min.js";
-      content = '<script type="text/javascript" src="' + src + '"></script>';
+      var src = "https://cdn.tinymce.com/"+ config['tinyMCE']['version'] +"/tinymce.min.js";
+      var sriHash = config['tinyMCE']['sriHash'];
+      content = '<script type="text/javascript" src="' + src + '" integrity="' + sriHash + '" crossorigin="anonymous"></script>';
     }
     return content;
   },
